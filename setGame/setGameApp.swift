9764ef1ff0ce8f574d673eb96db7c55b94c95b4b
Cody Mercadante <78059143+cody909@users.noreply.github.com>
2022-04-19 16:26:29 -0700

    Initial Commit

---
+++ setGame/setGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  setGameApp.swift
+//  setGame
+//
+//  Created by Cody Mercadante on 4/19/22.
+//
+
+import SwiftUI
+
+@main
+struct setGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
